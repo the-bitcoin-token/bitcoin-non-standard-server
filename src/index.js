11f9b05b4a201182251aab67bda1ddcbff927bba
Clemens Ley <ley.clemens@gmail.com>
2018-08-07 13:07:48 -0700

    added express server back in

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 // @flow
 
-import db from './db'
 
-export default db
+import express from 'express'
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
