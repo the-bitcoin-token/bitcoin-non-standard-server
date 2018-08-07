f7be496c8e8bccaa99e7198f7e56dd751b587ff1
Clemens Ley <ley.clemens@gmail.com>
2018-08-07 12:57:53 -0700

    added ad hoc schem import to pg-promise

--- src/db.js
+++ src/db.js
@@ -7,7 +7,7 @@ import pgPromise from 'pg-promise'
 export default class Db {
   static _db: Object
 
-  static init(): void {
+  static getConnection(): void {
     // make sure that only one db connection is created
     if (this._db) return
 
@@ -15,5 +15,29 @@ export default class Db {
     const pgp = pgPromise({})
     const cn = 'postgres://clemensley:@localhost:5432/unp2sh'
     this._db = pgp(cn)
+  }
+
+  static async createDataOutputsTable() {
+    const sql = `CREATE TABLE DataOutputs (
+      tx_id varchar(20) primary key,
+      public_key varchar(20) NOT NULL,
+      data_outputs text[] default NULL
+    );`
+    return this._db.none(sql)
+  }
+
+  static async dropDataOutputsTable() {
+    const sql = `DROP TABLE DataOutputs;`
+    return this._db.none(sql)
+  }
+
+  static async checkIfDataOutputsExists() {
+    const sql = `SELECT EXISTS (
+      SELECT 1
+      FROM   information_schema.tables 
+      WHERE  table_schema = 'public'
+      AND    table_name = 'dataoutputs'
+      );`
+    return this._db.one(sql)
   }
 }
--- test/db.test.js
+++ test/db.test.js
@@ -7,19 +7,82 @@ import Db from '../src/db'
 declare var describe: any
 declare var it: any
 declare var expect: any
+declare var beforeEach: any
+declare var afterEach: any
+declare var beforeAll: any
+declare var afterAll: any
 
-describe('init', async () => {
-  it.only('should create a new DB', async () => {
+describe('Db', async () => {
+  beforeAll(async () => {
     expect(Db._db).toBeUndefined()
-    Db.init()
+    Db.getConnection()
     expect(Db._db).toBeDefined()
+  })
 
-    // test that DB._db is working
-    const db = Db._db
-    const data = await db.one('SELECT $1 AS value', 123)
-    expect(data.value).toBe(123)
+  beforeEach(async () => {
+    try {
+      await Db.dropDataOutputsTable()
+    } catch (err) {
+      if (err.message !== 'table "dataoutputs" does not exist') {
+        throw err
+      }
+    }
+  })
 
-    // close connection pool
+  describe('getConnection', async () => {
+    it('should create a new DB', async () => {
+      // test that DB._db is working
+      const data = await Db._db.one('SELECT $1 AS value', 123)
+      expect(data.value).toBe(123)
+    })
+  })
+
+  describe('createDataOutputsTable', async () => {
+    it('should create the DataOutputs table', async () => {
+      await Db.createDataOutputsTable()
+      const data = await Db._db.none('SELECT * FROM DataOutputs;')
+      expect(data).toBe(null)
+    })
+
+    it('should throw an error if table is created twice', async () => {
+      await Db.createDataOutputsTable()
+
+      await expect(Db.createDataOutputsTable()).rejects.toThrow(
+        'relation "dataoutputs" already exists'
+      )
+    })
+  })
+
+  describe('dropDataOutputsTable', async () => {
+    it('should drop the DataOutputs table', async () => {
+      // querying DataOutputs should throw an error if it does not exist yet
+      await expect(Db._db.none('SELECT * FROM DataOutputs;')).rejects.toThrow(
+        'relation "dataoutputs" does not exist'
+      )
+
+      // querying DataOutputs should work after it is created
+      await Db.createDataOutputsTable()
+      const data = await Db._db.none('SELECT * FROM DataOutputs;')
+      expect(data).toBe(null)
+
+      // querying DataOutputs should throw an error after it has been dropped
+      await Db.dropDataOutputsTable()
+      await expect(Db._db.none('SELECT * FROM DataOutputs;')).rejects.toThrow(
+        'relation "dataoutputs" does not exist'
+      )
+    })
+
+    it('should throw an error if DataOutputs is dropped twice', async () => {
+      await Db.createDataOutputsTable()
+      await Db.dropDataOutputsTable()
+
+      await expect(Db.dropDataOutputsTable()).rejects.toThrow(
+        'table "dataoutputs" does not exist'
+      )
+    })
+  })
+
+  afterAll(async () => {
     Db._db.$pool.end()
   })
 })
