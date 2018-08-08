db7f5b5891cf2ec6ae708ffa6399d839d0767b03
Clemens Ley <ley.clemens@gmail.com>
2018-08-07 22:52:58 -0700

    added body-parser

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 // @flow
 
+/*eslint no-console: ["error", { allow: ["log"] }] */
+
 import express from 'express'
+import bodyParser from 'body-parser'
 
 const app = express()
+app.use(bodyParser.json())
 
-app.post('/dataOutputs', (req, res) => {
-  res.send(req.params)
-})
+app.post('/dataOutputs', (req, res) => res.send(req.body))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
