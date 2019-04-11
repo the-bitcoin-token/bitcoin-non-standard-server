51e387bc5190c63426e5a2b96bc98510c72d4ac1
Brenton Gunning <brentongunning@gmail.com>
2019-04-11 15:04:26 -0700

    Enable cors

--- src/app.js
+++ src/app.js
@@ -12,6 +12,13 @@ Db.getConnection()
 const app = express()
 app.use(bodyParser.json())
 
+// enable cors for the browser to talk to this server
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
+
 app.post('/', async (req: $Subtype<express$Request>, res: express$Response) => {
   try {
     // store in UnP2sh
