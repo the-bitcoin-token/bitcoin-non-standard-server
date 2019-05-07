a94382c1513cd70f37071d1e54b855d487caf594
Clemens Ley <ley.clemens@gmail.com>
2019-05-07 11:07:30 -0700

    refactored app.js

--- src/app.js
+++ src/app.js
@@ -27,32 +27,29 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
-app.post('/', async (req: $Request, res: $Response) => {
+app.post('/', async ({ body }: $Request, res: $Response) => {
   try {
-    if (!req.body || typeof req.body !== 'object')
+    if (!body || typeof body !== 'object')
       throw new Error('Parameter must be an object')
 
     // store in UnP2sh
-    const insertIntoUnP2sh =
+    const unp2shSql =
       'INSERT INTO UnP2sh(tx_id, output_data) VALUES (${tx_id}, ${output_data})'
-    await Db.none(insertIntoUnP2sh, objToSnakeCase(req.body))
+    await Db.none(unp2shSql, objToSnakeCase(body))
 
     // store in Txos
-    // $FlowFixMe
-    const { outputData, txId } = req.body
-    const outputDataObj = JSON.parse(outputData)
+    const { outputData, txId } = body
 
-    const insertIntoTxos =
+    // $FlowFixMe
+    const outputDataJson = JSON.parse(outputData)
+
+    const sqlTxos =
       'INSERT INTO Txos(public_key, tx_id, v_out, spent) VALUES (${publicKey}, ${txId}, ${vOut}, false)'
     await Promise.all(
-      outputDataObj.map(
-        async (element, index) =>
-          element.__kind === 'script'
-            ? Db.none(insertIntoTxos, {
-                txId,
-                vOut: index,
-                publicKey: element.__publicKeys[0]
-              })
+      outputDataJson.map(
+        async ({ __kind, __publicKeys: [publicKey] = [] }, vOut) =>
+          __kind === 'script'
+            ? Db.none(sqlTxos, { txId, vOut, publicKey })
             : Promise.resolve()
       )
     )
@@ -63,40 +60,40 @@ app.post('/', async (req: $Request, res: $Response) => {
   }
 })
 
-app.get('/un-p2sh/:txId', async (req: $Request, res: $Response) => {
+app.get('/un-p2sh/:txId', async ({ params }: $Request, res: $Response) => {
   try {
     const sql = 'SELECT output_data FROM UnP2sh WHERE tx_id = ${tx_id}'
-    const result: Array<any> = await Db.any(sql, objToSnakeCase(req.params))
+    const result = await Db.any(sql, objToSnakeCase(params))
     if (result.length && result[0].output_data) {
       const data = JSON.parse(result[0].output_data)
       res.status(200).json(data.map(objToCamelCase))
     } else {
-      throw new Error(`no data found for txId ${req.params.txId}`)
+      throw new Error(`no data found for txId ${params.txId}`)
     }
   } catch (err) {
     res.status(404).json({ error: err.message })
   }
 })
 
-app.post('/txos/set-spent/', async (req: $Request, res: $Response) => {
-  if (!req.body || typeof req.body !== 'object')
+app.post('/txos/set-spent/', async ({ body }: $Request, res: $Response) => {
+  if (!body || typeof body !== 'object')
     throw new Error('Parameter must be an object')
 
   try {
     const sql =
       'UPDATE Txos SET spent = true WHERE tx_id = ${tx_id} AND v_out = ${v_out}'
-    await Db.none(sql, objToSnakeCase(req.body))
+    await Db.none(sql, objToSnakeCase(body))
     res.status(201).json({})
   } catch (err) {
     res.status(500).json({ error: err.message })
   }
 })
 
-app.get('/txos/:publicKey', async (req: $Request, res: $Response) => {
+app.get('/txos/:publicKey', async ({ params }: $Request, res: $Response) => {
   try {
     const sql =
       'SELECT tx_id, v_out FROM Txos WHERE public_key = ${public_key} and spent = false'
-    const result: Array<any> = await Db.any(sql, objToSnakeCase(req.params))
+    const result: Array<any> = await Db.any(sql, objToSnakeCase(params))
     res.status(200).json(result.map(objToCamelCase))
   } catch (err) {
     res.status(404).json({ error: err.message })
